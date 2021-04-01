d0e7672592d03ee1d52f59d5fd54375607ac87bb
d-boy <gerry_gqj@outlook.com>
2021-04-01 23:04:54 +0800

    init commit

---
+++ src/components/main/Home.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="home">
+    <h1 style="text-align: center">W ! e ! l ! c ! o ! m ! e</h1>
+    <h1 style="text-align: center">W e l c o m e</h1>
+    <h1 style="text-align: center">W e l c o m e</h1>
+    <h1 style="text-align: center">W e l c o m e</h1>
+    <h1 style="text-align: center">W e l c o m e</h1>
+    <h1 style="text-align: center">W e l c o m e</h1>
+    <h1 style="text-align: center">W e l c o m e</h1>
+    <h1 style="text-align: center">W e l c o m e</h1>
+    <h1 style="text-align: center">W e l c o m e</h1>
+    <h1 style="text-align: center">W e l c o m e</h1>
+    <h1 style="text-align: center">W e l c o m e</h1>
+    <h1 style="text-align: center">W e l c o m e</h1>
+    <h1 style="text-align: center">W e l c o m e</h1>
+    <h1 style="text-align: center">W e l c o m e</h1>
+    <h1 style="text-align: center">W e l c o m e</h1>
+    <h1 style="text-align: center">W e l c o m e</h1>
+    <h1 style="text-align: center">W e l c o m e</h1>
+    <h1 style="text-align: center">W e l c o m e</h1>
+    <h1 style="text-align: center">W e l c o m e</h1>
+    <h1 style="text-align: center">W e l c o m e</h1>
+    <h1 style="text-align: center">W e l c o m e</h1>
+    <h1 style="text-align: center">W e l c o m e</h1>
+    <h1 style="text-align: center">W e l c o m e</h1>
+    <h1 style="text-align: center">W e l c o m e</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name:"home",
+  data() {
+    return {
+    };
+  },
+  methods: {
+
+
+  }
+}
+</script>
+
+<style>
+
+</style>
